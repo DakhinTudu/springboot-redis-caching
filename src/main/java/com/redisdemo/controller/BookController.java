d548f2b2eba479a533706429b071d17b6a6e554c
Dakhin <dtudu195@gmail.com>
2026-06-30 16:01:59 +0530

    Custom api response add

--- src/main/java/com/redisdemo/controller/BookController.java
+++ src/main/java/com/redisdemo/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.redisdemo.controller;
 
 import com.redisdemo.dto.ApiResponse;
+import com.redisdemo.dto.BookRequest;
+import com.redisdemo.dto.BookResponse;
+import com.redisdemo.dto.BookUpdateRequest;
 import com.redisdemo.entity.Books;
 import com.redisdemo.service.BookService;
 import com.redisdemo.utils.ApiResponseBuilder;
@@ -21,36 +24,33 @@ public class BookController {
     private final ApiResponseBuilder apiResponseBuilder;
 
     @PostMapping
-    public ResponseEntity<String> createBook(@RequestBody Books book) {
-        return ResponseEntity.ok(bookService.create(book));
+    public ResponseEntity<ApiResponse<BookResponse>> createBook(@RequestBody BookRequest book) {
+        return apiResponseBuilder.success(bookService.create(book),"Book create successfully",HttpStatus.CREATED);
     }
 
     @GetMapping("/{bookId}")
-    public ResponseEntity<Books> getBookById(@PathVariable UUID bookId) {
-        return ResponseEntity.ok(bookService.getBookById(bookId));
+    public ResponseEntity<ApiResponse<BookResponse>> getBookById(@PathVariable UUID bookId) {
+        return apiResponseBuilder.success(bookService.getBookById(bookId),"Records fetched successfully",HttpStatus.OK);
     }
 
     @GetMapping()
-    public ResponseEntity<ApiResponse<List<Books>>> getBooks(
+    public ResponseEntity<ApiResponse<List<BookResponse>>> getBooks(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
-
-
         return apiResponseBuilder
                 .success(bookService.getBooks(page, size), "Books fetched successfully", HttpStatus.OK);
     }
 
     @PutMapping("/{bookId}")
-    public ResponseEntity<String> updateBook(
+    public ResponseEntity<ApiResponse<BookResponse>> updateBook(
             @PathVariable UUID bookId,
-            @RequestBody Books book) {
-
-        return ResponseEntity.ok(bookService.update(bookId, book));
+            @RequestBody BookUpdateRequest book) {
+        return apiResponseBuilder.success(bookService.update(bookId, book),"Book updated successfully",HttpStatus.OK);
     }
 
     @DeleteMapping("/{bookId}")
-    public ResponseEntity<String> deleteBook(@PathVariable UUID bookId) {
+    public ResponseEntity<ApiResponse<Void>> deleteBook(@PathVariable UUID bookId) {
         bookService.delete(bookId);
-        return ResponseEntity.ok("Book Deleted Successfully");
+        return apiResponseBuilder.success(null,"Book deleted successfully",HttpStatus.NO_CONTENT);
     }
 }
